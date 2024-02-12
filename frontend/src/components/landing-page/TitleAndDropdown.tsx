672846bddfe776f892a6b0476e47134efdf2f95a
Venkatta <58058506+venkatta@users.noreply.github.com>
2024-02-12 16:24:55 +0000

    app configuration

--- frontend/src/components/landing-page/TitleAndDropdown.tsx
+++ frontend/src/components/landing-page/TitleAndDropdown.tsx
@@ -72,27 +72,24 @@ export const TitleAndDropdown = () => {
 
   return (
     <div className="landing-page-gradient-1 relative flex h-max w-screen flex-col items-center font-lora ">
-      <div className="absolute right-4 top-4">
+      <div className="absolute left-4 top-4">
         <a href="https://www.llamaindex.ai/" target="_blank">
           <button className="flex items-center justify-center font-nunito text-lg font-bold ">
-            Built by LlamaIndex
-            <img src="logo-black.svg" className="mx-2 rounded-lg" width={40} />
+            <img src="st-engineering.png" className="mx-2 rounded-lg" width={200} />
           </button>
         </a>
       </div>
       <div className="mt-28 flex flex-col items-center">
-        <div className="w-4/5 text-center text-4xl">
-          Empower your organization&apos;s Business Intelligence with{" "}
-          <span className="font-bold">SEC Insights </span>
+        <div className="text-center text-4xl">
+          <span className="font-bold">Financial Report Analyser </span>
         </div>
         <div className="mt-4 flex items-center justify-center">
           <div className="w-3/5 text-center font-nunito">
-            Effortlessly analyze multifaceted financial documents such as 10-Ks
-            and 10-Qs.
+
           </div>
         </div>
         <div className="mt-4 flex items-center justify-center">
-          <GitHubButton href="https://github.com/run-llama/sec-insights">Open-Sourced on Github</GitHubButton>
+
         </div>
       </div>
       {isMobile ? (
